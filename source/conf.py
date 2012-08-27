da0b1f6b0f233a9ed5410f08004fe18309b93520
Lian Cheng <rhythm.mail@gmail.com>
2012-08-27 22:42:13 +0800

    Added LLVM logo

--- source/conf.py
+++ source/conf.py
@@ -99,7 +99,8 @@ html_theme = 'rosefinch'
 html_theme_options = {
     'nosidebar': False,
     'rightsidebar': True,
-    'stickysidebar': True
+    'stickysidebar': True,
+    'siteurl': 'http://liancheng.github.com/llvm-tutorial-cn/',
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
@@ -114,7 +115,7 @@ html_title = u'LLVM教程'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = '_static/img/llvm-logo.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
