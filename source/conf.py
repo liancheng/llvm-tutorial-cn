fe53839c6c5e35a1c947aebc15a4aacfeb36b866
Lian Cheng <rhythm.mail@gmail.com>
2013-06-24 17:02:53 +0800

    Removed `rosefinch' submodule

--- source/conf.py
+++ source/conf.py
@@ -99,7 +99,7 @@ html_theme = 'nature'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = ['_themes/rosefinch']
+#html_theme_path = []
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
